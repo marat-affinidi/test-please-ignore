b4caabe0fa3e66cc41ac47909ab28fdd1f464a07
Marat Shakirov <marat.s@affinidi.com>
2026-03-02 12:46:40 +0100

    fix: reality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 function main() {
-  console.log('Hello from test-please-ignore');
+  console.log('Hello from test-please-always-ignore');
 }
 
 if (require.main === module) {
